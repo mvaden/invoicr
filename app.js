d56fc03a8792f08e0db3286d212f6e89ff052392
Wes Reed <wesley.a.reed@gmail.com>
2016-05-19 14:53:37 -0400

    Setup customers and invoice routes with directory

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var mongoose = require("mongoose");
 
 var app = express();
 
+var customers = require('./routes/customers');
+var invoices = require('./routes/invoices');
+
 // Mongoose Connect
 mongoose.connect('mongodb://localhost/invoicr');
 var db = mongoose.connection;
@@ -12,8 +15,11 @@ app.use(express.static(__dirname+'/client'));
 app.use(bodyParser.json());
 
 app.get('/', function(req, res){
-  res.send("Very Basic Express App")
+  res.send("Please use /api/customers or /api/invoices")
 });
+
+app.use('/api/customers', customers);
+app.use('/api/invoices', invoices);
 
 app.listen(3001, function(){
   console.log("started on port 3001...");
