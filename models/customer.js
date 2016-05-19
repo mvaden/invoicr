1ee52502e3635ba1492a320e84e240b789abbd4b
Wes Reed <wesley.a.reed@gmail.com>
2016-05-19 15:32:26 -0400

    Add customer model

---
+++ models/customer.js
@@ -0,0 +1,44 @@
+var mongoose = require("mongoose");
+
+// Customer Schema
+
+var customerSchema = mongoose.Schema({
+    first_name: {
+      type: String,
+      required: true
+    },
+    last_name: {
+      type: String,
+      required: true
+    },
+    company: {
+      type: String
+    },
+    logo_url: {
+      type: String
+    },
+    email: {
+      type: String,
+      required: true
+    },
+    phone: {
+      type: String
+    },
+    address: {
+      street: String,
+      city: String,
+      state: String,
+      zip: String
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now
+    }
+});
+
+var Customer = module.exports = mongoose.model('Customer', customerSchema);
+
+// Get Customer
+module.exports.getCustomers = function(callback, limit){
+  Customer.find(callback).limit(limit).sort([['first_name', 'ascending']])
+}
